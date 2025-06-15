4fc50dc746d5314ab3b5b197793d9473e53c3c6f
Ramyashri Padmanabhakumar <57267583+rum1887@users.noreply.github.com>
2025-06-15 13:43:58 +0530

    (bug): fix

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from .models import Base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+psycopg2://postgres:JPL2023@localhost:5432/test5"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
