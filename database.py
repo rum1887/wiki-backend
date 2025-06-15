b59d4aade7411900e56b8eac50332ed5b219f131
Ramyashri Padmanabhakumar <57267583+rum1887@users.noreply.github.com>
2025-06-15 14:50:06 +0530

    (bug): fix relative imports

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from models import Base
 from dotenv import load_dotenv
 import os
 
