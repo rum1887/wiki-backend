5839f828b0c3b8fec624e3928ffd0ef34bb1b085
Ramyashri Padmanabhakumar <57267583+rum1887@users.noreply.github.com>
2025-06-16 01:24:28 +0530

    (feat): supabase connection establishment changes

--- database.py
+++ database.py
@@ -8,6 +8,16 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
